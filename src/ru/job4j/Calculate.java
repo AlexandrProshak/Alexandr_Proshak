b3a03880e456ed85ec9ef379567838de39348465
Petr ArsentevPetr <olexandr_proshak@ukr.net>
2017-04-05 13:46:07 +0300

    Added Hello World

--- src/ru/job4j/Calculate.java
+++ src/ru/job4j/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j;
 
+/**
+* Calculate.
+*
+* @author Alex Proshak (olexandr_proshak@ukr.net)
+* @vershion $id$
+* @since 0.1
+*/
 public class Calculate {
+	/**
+	* Main.
+	* @param args - args.
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello World");
 	}
