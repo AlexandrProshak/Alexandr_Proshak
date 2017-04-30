a6138c8588739cf781878bca9defba0b09fbbf94
AlxMan <olexandr_proshak@ukr.net>
2017-04-30 23:12:04 +0300

    task 192 has remade according to the recommendations.

--- chapter_001/src/main/java/ru/job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -17,10 +17,8 @@ public class Counter {
 	public int add(int start, int finish) {
 		int sum = 0;
 		for (int i = 0; i <= 10; i++) {
-			if (i % 2 != 0) {
-				continue;
-			} else {
-				sum = sum + i;
+			if (i % 2 == 0) {
+				sum += i;
 			}
 		}
 		return sum;
