84f4731d01bfc4bf54e728096cafba3f7a48d499
Alexandr Proshak <mailto:olexandr_proshak@ukr.net>
2017-08-21 18:55:22 +0300

    Remade according to recommendations. (Additional test task of sorting an array of department codes.)

--- chapter_003/src/main/java/ru/job4j/collections/departments/DepartmentSort.java
+++ chapter_003/src/main/java/ru/job4j/collections/departments/DepartmentSort.java
@@ -1,9 +1,9 @@
 package ru.job4j.collections.departments;
 
-import java.util.Collection;
-import java.util.Set;
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.TreeSet;
+import java.util.Collections;
+import java.util.Collection;
 import java.util.Comparator;
 
 /**
@@ -18,14 +18,14 @@ public class DepartmentSort {
     /**
      * A set of unique departments name.
      */
-    private Set<String> departments;
+    private TreeSet<String> departments;
 
     /**
      * DepartmentSort class constructor.
      * @param list of departments to sort.
      */
     public DepartmentSort(Collection<String> list) {
-        this.departments = new HashSet<>();
+        this.departments = new TreeSet<>();
         for (String string: list) {
             addDepartments(string);
         }
@@ -49,9 +49,7 @@ public class DepartmentSort {
      * @return sorted collection in ascending order.
      */
     public Collection<String> ascendingDepSort() {
-        Set<String> result = new TreeSet<>();
-        result.addAll(this.departments);
-        return result;
+        return this.departments;
     }
 
     /**
@@ -60,37 +58,19 @@ public class DepartmentSort {
      * @return sorted collection in descending order.
      */
     public Collection<String> descendingDepSort() {
-        Set<String> result = new TreeSet<>(descending);
-        result.addAll(this.departments);
-        return result;
-    }
+        ArrayList<String> list = new ArrayList<>(this.departments);
 
-    /**
-     * Comparator for an descending order.
-     */
-    private Comparator<String> descending = new Comparator<String>() {
-        @Override
-        public int compare(String o1, String o2) {
-            int result = 0;
-            String[] st1 = o1.split("\\\\");
-            String[] st2 = o2.split("\\\\");
-
-            int departLength1 = st1.length;
-            int departLength2 = st2.length;
-            int minDepLength = Math.min(departLength1, departLength2);
-
-            result += (departLength1 - departLength2);
-            int index = 0;
-
-            while (index < minDepLength) {
-                if (!st1[index].equals(st2[index])) {
-                    result += st2[index].compareTo(st1[index]) * (1000 / (index + 1));
-                    break;
+        Collections.sort(list, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                int result = 0;
+                if (o1.compareTo(o2) == 1) {
+                    result = -o1.length() - o2.length();
                 }
-                index++;
+                return result;
             }
-            return result;
-        }
-    };
+        });
+        return list;
+    }
 }
 
