708a54be8fadf3ef830f7fa9be1748e1ff628cf6
Alexandr Proshak <olexandr_proshak@ukr.net>
2017-09-12 18:39:21 +0300

    3. Перекрывать hashCode. [#1003]

--- chapter_005/src/main/java/ru/job4j/map/UserMap.java
+++ chapter_005/src/main/java/ru/job4j/map/UserMap.java
@@ -18,17 +18,17 @@ public class UserMap {
         /**
          * Name of user.
          */
-        String name;
+        private String name;
 
         /**
          * Amount of children.
          */
-        int children;
+        private int children;
 
         /**
          * Date of born.
          */
-        Calendar birthday;
+        private Calendar birthday;
 
         /**
          * Constructor.
@@ -37,10 +37,22 @@ public class UserMap {
          * @param children amount of children.
          * @param birthday date of born.
          */
-        public User(String name, int children, Calendar birthday) {
+        User(String name, int children, Calendar birthday) {
             this.name = name;
             this.children = children;
             this.birthday = birthday;
+        }
+
+        /**
+         * Overloaded method hashCode.
+         * @return hashCode related from field of class user.
+         */
+        @Override
+        public int hashCode() {
+            int result = name != null ? name.hashCode() : 0;
+            result = 31 * result + children;
+            result = 31 * result + (birthday != null ? birthday.hashCode() : 0);
+            return result;
         }
     }
 
@@ -49,8 +61,10 @@ public class UserMap {
      * @param args arguments.
      */
     public static void main(String[] args) {
-        User firstUser = new User("John", 2,  new GregorianCalendar(1896, Calendar.JULY, 14));
-        User secondUser = new User("John", 2,  new GregorianCalendar(1896, Calendar.JULY, 14));
+        User firstUser = new User(
+                "John", 2,  new GregorianCalendar(1896, Calendar.JULY, 14));
+        User secondUser = new User(
+                "John", 2,  new GregorianCalendar(1896, Calendar.JULY, 14));
 
         Map<User, Object> map = new HashMap<>();
 
