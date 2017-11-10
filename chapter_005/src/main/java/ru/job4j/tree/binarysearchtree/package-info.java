bf8f2999906114a9e23cd26a6f1b1525881128d7
AlxMan <olexandr_proshak@ukr.net>
2017-11-10 22:17:10 +0200

    3. Собственная реализация Binary search tree. [#1714]

---
+++ chapter_005/src/main/java/ru/job4j/tree/binarysearchtree/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package for the third task of the tree section.
+ */
+package ru.job4j.tree.binarysearchtree;
---
+++ chapter_005/src/test/java/ru/job4j/tree/binarysearchtree/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * The package for tests of the third tree task.
+ */
+package ru.job4j.tree.binarysearchtree;
