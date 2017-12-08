1064bd7ceba268bc384a5c1c56744375fa9af169
Alexandr Proshak <olexandr_proshak@ukr.net>
2017-12-08 14:58:42 +0200

    2. Сделать ожидание вывода. [#1017]

--- chapter_006/src/main/java/ru/job4j/threads/Runner.java
+++ chapter_006/src/main/java/ru/job4j/threads/Runner.java
@@ -12,10 +12,22 @@ public class Runner {
         String string = "My name is john, and I like Java";
         SpacingCounter spaceCounter = new SpacingCounter(string);
         WordCounter wordCounter = new WordCounter(string);
-        System.out.println(string);
-        new Thread(spaceCounter).start();
-        new Thread(wordCounter).start();
-        System.out.println("Finish a method main.");
+        System.out.println("    The program counts words and whitespaces in a sentence.");
+        System.out.println();
+        System.out.println(" '' " + string + " '' ");
+        System.out.println();
+        Thread spaceCounterThread = new Thread(spaceCounter);
+        Thread wordCounterThread = new Thread(wordCounter);
+        spaceCounterThread.start();
+        wordCounterThread.start();
+        try {
+            spaceCounterThread.join();
+            wordCounterThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println();
+            System.out.println("    Finish program.");
+        }
     }
-
 }
