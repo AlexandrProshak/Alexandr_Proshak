9e0a3ef384b1ea646c109120ca8d8788c5937817
Alexandr Proshak <olexandr_proshak@ukr.net>
2018-02-08 18:22:04 +0200

    1. Реализовать шаблон Producer Consumer. [#1098]

---
+++ chapter_006/src/main/java/ru/job4j/waitnotifynotifyall/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * The package waitnotifynotifyall is for the tasks of a Wait, Notify, NotifyAll module.
+ */
+package ru.job4j.waitnotifynotifyall;
---
+++ chapter_006/src/test/java/ru/job4j/waitnotifynotifyall/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package for the wait, notify, notifyAll tasks.
+ */
+package ru.job4j.waitnotifynotifyall;
